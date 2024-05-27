18593be2073c92bf86c08478e728fc94cfeb22db
Eroxl <InfinityWaffles@outlook.com>
2024-05-27 10:36:04 -0700

    Fix list styling being cut off

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,14 +11,14 @@ const List: React.FC<ListProps> = (props) => {
 
   return (
     <ul
-      className="list-disc list-inside pl-4 marker:text-[#4c566a]"
+      className="list-disc list-inside pl-4 w-full marker:text-[#4c566a]"
     >
       <li>
         <Content textContent={node.properties['content'] as string} />
       </li>
 
       <div
-        className="pl-4 ml-[0.23rem] w-min border-l border-[#4c566a]"
+        className="pl-4 ml-[0.23rem] w-full border-l border-[#4c566a]"
       >
         {
           node.properties['subNodes'] && (node.properties['subNodes'] as TextNode[])?.map((subNode) => (
